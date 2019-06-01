a24353cdf3d265d1f9fa0433f3a4554439f79127
J Cover <royalcows9@gmail.com>
2019-06-01 13:52:34 -0700

    Added registration and checkin data
    Now will add docker running

--- srndchalengepackage/main/routes.py
+++ srndchalengepackage/main/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template
+from sqlalchemy.sql import label
 from sqlalchemy.sql.functions import sum, count
 
 from srndchalengepackage.main import bp
+import datetime as dt
 
 
 @bp.route("/")
@@ -14,6 +16,14 @@ def data():
     from srndchalengepackage.models import Registrations as Reg, Sponsors
     from srndchalengepackage import db
 
+    # Some stuff that's useful in a few places:
+    region_list = (
+        db.session.query(Reg.region_name)
+        .distinct(Reg.region_name)
+        .group_by(Reg.region_name)
+        .all()
+    )
+
     # Calculate the ticket related info
     ticket_sales = (
         db.session.query(sum(Reg.ticket_cost)).scalar() * 0.01
@@ -24,27 +34,31 @@ def data():
     food_costs = db.session.query(Reg).count() * 11  # Food cost = $11 per attendee
     net_costs = ticket_sales + sponsor_bucks - food_costs
 
+    # ------------------------------------------------------------------------------------------------------------------
+
     # Calculate the early bird related info
     early_birds = {}
-    for event in (
-        db.session.query(Reg.region_name)
-        .distinct(Reg.region_name)
-        .group_by(Reg.region_name)
-        .all()
-    ):
+    for event in region_list:
         total_attendee_count = (
             db.session.query(Reg).filter(Reg.region_name == event[0]).count()
         )
         early_bird_count = (
             db.session.query(Reg)
-            .filter(Reg.region_name == event[0])
+            .filter(
+                Reg.region_name == event[0],
+                Reg.registered_at
+                <= dt.datetime.fromtimestamp(1550318400) - dt.timedelta(weeks=2),
+            )
             .limit(total_attendee_count * 0.4)
             .count()
         )
-        early_birds[event.region_name] = early_bird_count
+        early_birds[event.region_name] = (early_bird_count, total_attendee_count)
     print(early_birds)
 
+    # ------------------------------------------------------------------------------------------------------------------
+
     # Calculates the promo related info
+    # Returns the number of uses of each promo code
     promo_data = (
         db.session.query(Reg.promo_code, count(Reg.promo_code).label("num_uses"))
         .group_by(Reg.promo_code)
@@ -53,6 +67,35 @@ def data():
     )
     print(promo_data)
 
+    # ------------------------------------------------------------------------------------------------------------------
+    # Calculates attendance and registration info per region
+    checked_registered_data = (
+        db.session.query(
+            Reg.region_name,
+            count(Reg.checked_in_at).label("num_checked_in"),
+            count(Reg.registered_at).label("num_registered"),
+        )
+        .group_by(Reg.region_name)
+        .all()
+    )
+
+    checked_registered_data = (
+        checked_registered_data
+        + db.session.query(
+            Reg.first_name,
+            count(Reg.checked_in_at).label("num_checked_in"),
+            count(Reg.registered_at).label("num_registered"),
+        ).all()
+    )
+
+    print(checked_registered_data)
+
+    # ------------------------------------------------------------------------------------------------------------------
+
     return render_template(
-        "results.html", netcosts=net_costs, earlybirds=early_birds, promos=promo_data
+        "results.html",
+        netcosts=net_costs,
+        earlybirds=early_birds,
+        promos=promo_data,
+        registration_info=checked_registered_data,
     )
